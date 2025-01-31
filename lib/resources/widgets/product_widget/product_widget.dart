43f383ff3ca56a98ce5dadf175909148fa07f773
Ali Raza <ali.razaa@devsinc.com>
2025-01-31 19:43:17 +0500

    product card fixes

--- lib/resources/widgets/product_widget/product_widget.dart
+++ lib/resources/widgets/product_widget/product_widget.dart
@@ -25,82 +25,87 @@ class ProductWidget extends StatelessWidget {
         width: 145.w,
         // height: 370,
         // margin: EdgeInsets.only(left: 10.w),
-        decoration: BoxDecoration(
-            // border: Border.all(
-            // color: AppColors.black1C.withOpacity(0.5),
-            // width: 0.5,
-            // ),
-            ),
+        decoration: BoxDecoration(),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                  // borderRadius: BorderRadius.circular(5),
-                  ),
-              child: AspectRatio(
-                aspectRatio: 6 / 7,
-                child: CachedNetworkImage(
-                  fit: BoxFit.fill,
-                  imageUrl: singleProduct.variants.edges[0].node.image.url,
-                  // 'https://pixlr.com/images/generator/photo-generator.webp',
-                  // height: 200.h,
-                  placeholder: (context, url) => SizedBox(
-                    width: double.infinity,
-                    height: 202.h,
-                    child: Center(
-                      child: Image(
-                        image: AssetImage(
-                          AppImages.logoImage,
+            AspectRatio(
+              aspectRatio: 6/8,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: CachedNetworkImage(
+                      fit: BoxFit.fill,
+                      imageUrl: singleProduct.variants.edges[0].node.image.url,
+                      // 'https://pixlr.com/images/generator/photo-generator.webp',
+                      // height: 200.h,
+                      placeholder: (context, url) => SizedBox(
+                        width: double.infinity,
+                        height: 202.h,
+                        child: Center(
+                          child: Image(
+                            image: AssetImage(
+                              AppImages.logoImage,
+                            ),
+                          ),
                         ),
                       ),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
                     ),
                   ),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                ),
-              ),
-            ),
-            // ----button----
-            Positioned(
-              bottom: 10,
-              child: SizedBox(
-                height: 24,
-                width: 24,
-                child: Center(
-                  child: FloatingActionButton(
-                    heroTag: "pl",
-                    backgroundColor: AppColors.myScaffold,
-                    elevation: 0,
-                    isExtended: false,
-                    mini: true,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100)),
-                    child: Icon(
-                      Icons.add,
-                      color: AppColors.myPrimary,
-                      size: 12.h,
-                      weight: 700,
-                    ),
-                    onPressed: () {
-                      showModalBottomSheet(
-                        context: context,
-                        clipBehavior: Clip.antiAlias,
-                        builder: (context) => ProductBottomSheetWidget(
-                          singleProduct: singleProduct,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              // BorderRadius.only(
-                              //   topLeft: Radius.circular(0),
-                              //   topRight: Radius.circular(20),
-                              // ),
+                  Positioned(
+                    bottom: 10,
+                    left: 0,
+                    right: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        SizedBox(
+                          height: 24,
+                          width: 24,
+                          child: Center(
+                            child: FloatingActionButton(
+                              heroTag: "pl",
+                              backgroundColor: AppColors.myScaffold,
+                              elevation: 0,
+                              isExtended: false,
+                              mini: true,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100)),
+                              child: Icon(
+                                Icons.add,
+                                color: AppColors.myPrimary,
+                                size: 12.h,
+                                weight: 700,
+                              ),
+                              onPressed: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  clipBehavior: Clip.antiAlias,
+                                  builder: (context) =>
+                                      ProductBottomSheetWidget(
+                                    singleProduct: singleProduct,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        // BorderRadius.only(
+                                        //   topLeft: Radius.circular(0),
+                                        //   topRight: Radius.circular(20),
+                                        // ),
 
-                              BorderRadius.vertical(top: Radius.circular(16)),
+                                        BorderRadius.vertical(
+                                            top: Radius.circular(16)),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
                         ),
-                      );
-                    },
+                      ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
             // ---- sales tag----
@@ -120,77 +125,57 @@ class ProductWidget extends StatelessWidget {
             //   ),
             // ),
 
-            // ----- On tap ------
-            Positioned(
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: InkWell(
-                onTap: () {
-                  context.push("/productDetailScreen");
-                },
+            Container(
+              width: 57.w,
+              height: 31.h,
+              margin: EdgeInsets.only(top: 7.h, bottom: 12.h),
+              decoration: BoxDecoration(
+                color: AppColors.greyCA,
+                borderRadius: BorderRadius.circular(70.r),
+              ),
+              child: Center(
+                child: Text(
+                  'Infinity',
+                  style: AppTextStyles.body2,
+                ),
               ),
             ),
-            // ],
-            // ),
-            // ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 57.w,
-                  height: 31.h,
-                  margin: EdgeInsets.only(top: 7.h, bottom: 12.h),
-                  decoration: BoxDecoration(
-                    color: AppColors.greyCA,
-                    borderRadius: BorderRadius.circular(70.r),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Infinity',
-                      style: AppTextStyles.body2,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 100.w,
-                  child: Text(
-                    "Infinity Women's Split Neck Top…",
+            SizedBox(
+              width: 100.w,
+              child: Text(
+                "Infinity Women's Split Neck Top…",
+                style: AppTextStyles.body2.copyWith(fontSize: 12.sp),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.h),
+              child: Row(
+                children: [
+                  Text(
+                    "Color Name",
                     style: AppTextStyles.body2.copyWith(fontSize: 12.sp),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.h),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Color Name",
-                        style: AppTextStyles.body2.copyWith(fontSize: 12.sp),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 5.w),
-                        // width: 125.w,
-                        child: Text(
-                          "06 Colors",
-                          style: AppTextStyles.body3.copyWith(fontSize: 12.sp),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5.h),
-                  // width: 125.w,
-                  child: Text(
-                    "SAR 350",
-                    style: AppTextStyles.headline2.copyWith(
-                      fontSize: 17.sp,
-                      fontWeight: FontWeight.bold,
+                  Padding(
+                    padding: EdgeInsets.only(left: 5.w),
+                    // width: 125.w,
+                    child: Text(
+                      "06 Colors",
+                      style: AppTextStyles.body3.copyWith(fontSize: 12.sp),
                     ),
                   ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 5.h),
+              // width: 125.w,
+              child: Text(
+                "SAR 350",
+                style: AppTextStyles.headline2.copyWith(
+                  fontSize: 17.sp,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
+              ),
             ),
           ],
         ),
